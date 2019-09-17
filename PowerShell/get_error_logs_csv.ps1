05bda55b58cc1c46d073acfd4f60f910d9ad6317
KevGue <55241227+KevGue@users.noreply.github.com>
2019-09-17 16:54:17 +0200

    Cleanup

--- PowerShell/get_error_logs_csv.ps1
+++ PowerShell/get_error_logs_csv.ps1
@@ -1,12 +1,13 @@
+#Only a few logs are checked
 $relevantlogs = @('Application','System', 'Security')
 
-
+#The timeframe to check
 $enddate = (Get-Date).Date
 $startdate = (Get-Date).AddDays(-7).Date
 
-#$lognames = Get-WinEvent -ListLog * | Select-Object LogName
-
+#Loop through each log and...
 foreach ($log in $relevantlogs) {
+    #Check the events
     Get-WinEvent -LogName $log | Where-Object {($_.timecreated.date -le $enddate -and $_.timecreated.date -ge $startdate) -and `
          ($_.leveldisplayname -eq "Error" -or $_.leveldisplayname -eq "Fehler")} | Export-Csv -Path C:\temp\powershell\logs.csv
 
