bc777da72476e70e9db5fa1a2d130d9375b0af49
KevGue <55241227+KevGue@users.noreply.github.com>
2019-12-17 11:25:24 +0100

    Connect and copy to backup share

---
+++ PowerShell/backup_copy_share.ps1
@@ -0,0 +1,5 @@
+$encrypted = Get-Content <PATH_TO_SECURE_PASSWORD> | ConvertTo-SecureString
+$credential = New-Object System.Management.Automation.PsCredential("veeambkp", $encrypted)
+New-PSDrive -name "S" -PSProvider FileSystem -Root <NETWORKSHARE> -Persist -Credential $credential
+Copy-Item <SOURCE_DIR> <DEST_DIR> -Recurse
+Remove-PSDrive -name "S" -Force
