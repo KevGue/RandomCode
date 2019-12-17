71f045f3ebc14a9e6d8096c2717be5de5d623b0f
KevGue <55241227+KevGue@users.noreply.github.com>
2019-12-17 11:22:50 +0100

    Force logoff on RDS except for administrator

---
+++ PowerShell/rds_user_force_logoff.ps1
@@ -0,0 +1,6 @@
+﻿import-module remotedesktopservices
+$sessions = Get-RDUserSession | Where-Object {$_.username -ne "administrator"}
+foreach ($session in $sessions) {
+    
+    invoke-rduserlogoff -hostserver <SERVER> -UnifiedSessionID $session.unifiedsessionid -Force
+}
