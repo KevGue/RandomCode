286234722508317b66a41a4756493470c6423b7d
KevGue <55241227+KevGue@users.noreply.github.com>
2020-01-13 09:34:42 +0100

    Changed variables

--- PowerShell/backup_copy_share.ps1
+++ PowerShell/backup_copy_share.ps1
@@ -1,5 +1,5 @@
 $encrypted = Get-Content <PATH_TO_SECURE_PASSWORD> | ConvertTo-SecureString
-$credential = New-Object System.Management.Automation.PsCredential("veeambkp", $encrypted)
+$credential = New-Object System.Management.Automation.PsCredential("<ACCOUNTNAME>", $encrypted)
 New-PSDrive -name "S" -PSProvider FileSystem -Root <NETWORKSHARE> -Persist -Credential $credential
 Copy-Item <SOURCE_DIR> <DEST_DIR> -Recurse
 Remove-PSDrive -name "S" -Force
