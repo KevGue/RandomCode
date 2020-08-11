14b0aa353ba2fa7c363d2fb416d978ff8c0b34b5
KevGue <55241227+KevGue@users.noreply.github.com>
2020-08-11 08:19:23 +0200

    Copies folders according to regex

---
+++ PowerShell/copy_specific_folders.ps1
@@ -0,0 +1,9 @@
+﻿$source = "C:\temp\random"
+$destination = "C:\temp\powershell"
+$folders = get-childitem -path $source
+
+foreach ($folder in $folders) {
+    if ($folder.basename -match "[a-dA-D].*") {
+        robocopy $folder.FullName $destination\$folder /E /R:1 /W:1 /V
+    }
+}
